ba7b8341c86d47138c40669df9564d490ff07a39
toyo009 <issei-toyomoto-k22h@stu.kbc.ac.jp>
2025-09-06 14:38:31 +0900

    [add] 通信で使うクラス、関数の追加

---
+++ source/system/TCP/TCP_Module.cpp
@@ -0,0 +1,73 @@
+﻿#include "TCP_Module.h"
+
+bool TCP_Module::Winsock_Init(int port, String addr)
+{
+	//Winsock2の初期化
+	int initState = WSAStartup(MAKEWORD(2, 0), &wsaData);
+	if (initState != 0) {//winsock2の初期化に失敗
+		printf_s("winsock init....fail\n");
+		printf_s("winsock state....%d\n", initState);
+		return false;
+	}
+	//ソケットの作成
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	//接続先指定用構造体の準備
+	server.sin_family = AF_INET;
+	server.sin_port = htons(port);
+
+	//Siv3D String(UTF-32) -> std::wstring(UTF-16)　※変換
+	std::wstring waddr = Unicode::ToWstring(addr);
+	//IPアドレスをテキスト形式からバイナリ形式に変換
+	InetPton(server.sin_family, waddr.c_str(), &server.sin_addr.S_un.S_addr);
+
+	printf_s("winsock init....succeed\n");
+	return true;
+}
+
+void TCP_Module::Winsock_End(void)
+{
+	// Winsockの終了処理
+	WSACleanup();
+	printf("winsock cleanup done.\n");
+}
+
+int TCP_Module::ConnectServer(void)
+{
+	int connectState;
+	connectState = connect(sock, (struct sockaddr*)&server, sizeof(server));
+	if (connectState != 0) {
+		printf_s("connect fail\n");
+		return -1;
+	}
+	else {
+		printf_s("connect success\n");
+		return 0;
+	}
+
+	return -1;
+}
+
+std::string TCP_Module::getData()
+{
+	char buf[32];
+	//サーバからデータを受信
+	memset(buf, 0, sizeof(buf));
+	int n = recv(sock, buf, sizeof(buf), 0);
+	printf_s("Received from server %d %s\n", n, buf);
+
+	return buf;
+}
+
+void TCP_Module::sendData(std::string sd)
+{
+	//string型 -> char型
+	char buf[32];
+	strcpy_s(buf, sizeof(buf), sd.c_str());
+
+	//サーバに送信
+	int n = send(sock, buf, 3, 0);
+	if (n < 1) {//送信エラーが発生した場合はソケットを閉じる
+		printf_s("send : %d\n", WSAGetLastError());
+		closesocket(sock);
+	}
+}
---
+++ source/system/TCP/TCP_Module.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+//サーバ・クライアント間で通信する際に使用するクラス
+
+//通信
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+//_Tマクロ使用
+#include <tchar.h>
+
+class TCP_Module
+{
+private:
+	WSADATA wsaData;
+	struct sockaddr_in server;
+	SOCKET sock;
+public:
+	/**
+	 * @fn
+	 * Winsock_Init
+	 * @brief winsockの初期化
+	 * @param (port) ポート番号をここに入れる
+	 * @param (addr) アドレスをString型で入れる
+	 * @return winsockの初期化が成功したか、成功した場合はTureが返される
+	 * @detail winsockの初期化、ソケットの作成、IPアドレスのバイナリ変換をこの関数で行う
+	*/
+	bool Winsock_Init(int port, String addr);
+	/**
+	 * @fn
+	 * Winsock_End
+	 * @brief winsockの終了処理
+	 * @detail winsockの終了処理
+	*/
+	void Winsock_End(void);
+	/**
+	 * @fn
+	 * ConnectServer
+	 * @brief サーバに接続
+	 * @return サーバに接続成功した場合は0、失敗した場合は-1
+	 * @detail winsockを初期化時に設定したポートとIPからサーバに接続する
+	*/
+	int ConnectServer(void);
+	/**
+	 * @fn
+	 * getData
+	 * @brief サーバからデータを取得する
+	 * @return サーバからのString型のデータ
+	 * @detail サーバからのデータをString型でデータを返す
+	*/
+	std::string getData(void);
+	/**
+	 * @fn
+	 * sendData
+	 * @brief サーバへデータを送信する
+	 * @param (sd) サーバへ送信したいデータ（String型）
+	 * @detail サーバへchar型でデータを送信する
+	*/
+	void sendData(std::string sd);
+};
+
