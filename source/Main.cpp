a914537fe95b03273e783ed332aa5218d8c35b26
toyo009 <issei-toyomoto-k22h@stu.kbc.ac.jp>
2025-10-10 09:48:02 +0900

    [update] winsockからsiv3dのTCPに変更

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,5 +1,4 @@
 ﻿#include <Siv3d.hpp>
-#include "system/TCP/TCP_Module.h"
 #include "Login/Login.h"
 
 #define PORT 12345
@@ -8,35 +7,47 @@ void Main()
 {
 	Window::SetTitle(U"DCG Server");
 	Window::Resize(800, 600);
-	TCP_Module tcp;
-
-	//winsockの初期化
-	tcp.Winsock_Init(PORT);
-	// フォントをあらかじめ作成（毎フレーム作らない）
-	Font font(20);
 	// 背景色を塗る
 	Scene::SetBackground(ColorF{ 0.2, 0.4, 0.6 });
+	// フォントをあらかじめ作成（毎フレーム作らない）
+	Font font(20);
+
+	//利用するポートを開放
+	const uint16 port = 50000;
+	//クライアントとの接続状態を管理するフラグ
+	bool connected = false;
+	//TCPサーバーオブジェクトを作成し、指定ポートで接続受付を開始する
+	TCPServer server;
+	server.startAccept(port);
+	//ウィンドウタイトルに初期状態（接続待機中）を表示
+	Window::SetTitle(U"TCPServer: Waiting for connection...");
+	
 
 	while (System::Update())
 	{
-		//クライアントからのデータを受信
-		String getData = tcp.AcceptConnection();
+		//クライアントからの接続セッションが存在する場合
+		if (server.hasSession()) {
+			//初回の接続確立時に接続状態フラグを更新し、ウィンドウタイトルを変更する
+			if (not connected) {
+				connected = true;
+				Window::SetTitle(U"TCPServer: Connection established!");
+			}
+			//クライアントへ送信
+			//server.send();
 
-		// 受け取ったデータをJSON形式に変換
-		JSON json = JSON::Parse(getData);
-
-		//受信したデータからどの関数、メソッドで処理するか判定する
-		String method = json[U"method"].getString();//methodの値をString型に変換
-
-		if (method == U"Login") {//ログイン処理
-			Print << U"Login処理を開始";
-			Login login;
+			//クライアントから送信されたデータを受信する
+			//（複数データが同時に送られてくる可能性に備えて、すべてのデータを読み込む）
+			//while (server.read());
 		}
-		else if (method == U"") {
 
+		//接続済みだが、セッションが切断された場合の処理
+		if (connected && (not server.hasSession())) {
+			//セッション切断処理を実行する
+			server.disconnect();
+			connected = false;
+			//ウィンドウタイトルを接続待機中に変更し、再度接続受付を開始する
+			Window::SetTitle(U"TCPServer: Waiting for connection...");
+			server.startAccept(port);
 		}
 	}
-
-	//winsockの終了処理
-	tcp.Winsock_End();
 }
