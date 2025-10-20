37e9f263c463c3292f5ae7e0b2567479fefa2861
toyo009 <issei-toyomoto-k22h@stu.kbc.ac.jp>
2025-10-20 15:05:22 +0900

    [update] jsonファイルの読み込み成功

--- source/Login/Login.cpp
+++ source/Login/Login.cpp
@@ -3,10 +3,29 @@
 String Login::CreateLoginID(JSON playerInfo)
 {
 	//PlayerDBのJSONを読み込む
-	JSON playerDB = JSON::Load(U"../system/JSON/data/DBPlayer.json");
+	JSON playerDB = JSON::Load(U"../source/system/JSON/data/DBPlayer.json");
 
-	//
-	
+	//PlayerDBを読み込めたか判定
+	if (playerDB.isEmpty()) {
+		return U"PlayerDBを読み込めませんでした";
+	}
+	else {
+		String te = playerDB.format();
+		Print << U"PlayerDBの読み込み成功";
+	}
 
-	return String();
+	return playerDB.format();
+}
+
+String Login::GenerateLoginID(size_t length)
+{
+	static const String chars = U"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	String result;
+
+	for (size_t i = 0; i < length; ++i)
+	{
+		result += chars[Random(chars.size() - 1)];
+	}
+
+	return result;
 }
--- source/Login/Login.h
+++ source/Login/Login.h
@@ -1,11 +1,15 @@
 ﻿#pragma once
 #include <Siv3d.hpp>
 
+#define LOGINID_CHRA_COUNT 10
+
 class Login
 {
 private:
+	String GenerateLoginID(size_t length);
 
 public:
 	String CreateLoginID(JSON playerInfo);
+
 };
 
